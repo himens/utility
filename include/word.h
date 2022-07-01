349b921c0b101b19e0d463f68fe62eca5ded108f
Stefano Gallorini <himens@gmail.com>
2022-07-01 08:35:58 +0200

    Try to fix issue with negative numbers.

--- include/word.h
+++ include/word.h
@@ -23,8 +23,7 @@ class Word
 
     // bitset_t type definition
     typedef std::bitset<SIZE> bitset_t;
-    typedef int16_t mil_t;
-    typedef uint16_t umil_t;
+    typedef uint16_t mil_t;
 
     // constructors
     Word() {}
@@ -49,91 +48,95 @@ class Word
     // reset word bits to zero
     void reset() { _word.reset(); }
 
-    // reverse word bits between [lsb, msb]
-    void reverse_bits(const size_t lsb = 0, const size_t msb = SIZE - 1) { reverse_bits(_word, lsb, msb); }
+    // reverse word bits 
+    void reverse_bits(const size_t lsb = 0, const size_t size = SIZE) { reverse_bits(_word, lsb, size); }
 
     // get word to MIL type (signed/unsigned)
     mil_t to_mil() const { return static_cast<mil_t>(_word.to_ulong()); }
-    umil_t to_umil() const { return static_cast<umil_t>(_word.to_ulong()); }
     
-    // get data in [lsb, msb] to MIL type
-    mil_t to_mil(const size_t lsb, const size_t msb, const float msb_value) const
+    // get data to MIL type
+    mil_t to_mil(const size_t lsb, const size_t size, const float msb_value) const
     {
-      check_bit_range(lsb, msb);
+      check_bit_range(lsb, size);
 
-      const size_t n_bits = msb - lsb + 1;
-
-      bitset_t bits = _word & get_mask(lsb, msb); // extract bits in [lsb, msb]
+      bitset_t bits = _word & get_mask(lsb, size); // extract bits 
       bits >>= lsb; // shift back to bit 0
-      mil_t mil_data = static_cast<mil_t>(bits.to_ulong());
+      mil_t mil = static_cast<mil_t>(bits.to_ulong());
 
-      if (bits[n_bits - 1] == 1) mil_data -= (1 << n_bits); // if sign bit is set, mil is negative
-
-      return mil_data;
+      return mil;
     }
 
-    // get data stored in [lsb, msb] to type T
+    // get data to type T
     template <typename T>
-    T get_data(const size_t lsb, const size_t msb, const float msb_value) const 
+    T get_data(const size_t lsb, const size_t size, const float msb_value) const 
     {
-      check_bit_range(lsb, msb);
+      check_bit_range(lsb, size);
 
-      const size_t n_bits = msb - lsb + 1;
-      const float lsb_value = std::abs(msb_value) / (1 << (n_bits - 1));
+      const float lsb_value = get_lsb_value(size, msb_value);
+      int mil = to_mil(lsb, size, msb_value);
 
-      mil_t mil_data = to_mil(lsb, msb, msb_value);
+      if (msb_value < 0) mil -= (1 << size); // it's a negative number
 
-      return static_cast<T>(mil_data * lsb_value);
+      T data = static_cast<T>(mil * lsb_value);
+
+      return data;
     }
 
     // swap word bytes
     void swap_bytes() { _word = bitset_t(__bswap_16(_word.to_ulong())); }
 
-    // put data to word location [lsb, msb] in MIL format
+    // put data to word in MIL format
     template <typename T>
-    void put_data(const T &data, const size_t lsb, const size_t msb, const float msb_value, const bool reverse = false)
+    void put_data(const T &data, const size_t lsb, const size_t size, const float msb_value, const bool reverse = false)
     {
-      check_bit_range(lsb, msb);
+      check_bit_range(lsb, size);
 
-      const size_t n_bits = msb - lsb + 1;
-      const float lsb_value = std::abs(msb_value) / (1 << (n_bits - 1));
+      const float lsb_value = get_lsb_value(size, msb_value);
+      int mil = static_cast<int>(data / lsb_value) + (1 << size);
+      bitset_t bits(mil);
+      bits = (bits << lsb) & get_mask(lsb, size); // shift bits by lsb and mask the rest above msb
 
-      bitset_t bits(static_cast<mil_t>(data / lsb_value));
-      bits = (bits << lsb) & get_mask(lsb, msb); // shift bits by lsb and mask the rest above msb
-
-      if (reverse) reverse_bits(bits, lsb, msb);
+      if (reverse) reverse_bits(bits, lsb, size);
 
       _word |= bits;
     }
 
   private:
-    // check bit range [lsb, msb]
-    void check_bit_range(const size_t lsb, const size_t msb) const
+    // check bit range 
+    void check_bit_range(const size_t lsb, const size_t size) const
     {
+      size_t msb = lsb + size - 1;
       if (lsb > msb) throw std::range_error("Word::check_bit_range: lsb greater than msb!");
       if (lsb < 0 || msb > (SIZE - 1)) throw std::out_of_range("Word::check_bit_range: lsb or msb out-of-range!");
     }
 
-    // get mask for bits [lsb, msb]
-    bitset_t get_mask(const size_t lsb, const size_t msb) const
+    // get mask for bit interval
+    bitset_t get_mask(const size_t lsb, const size_t size) const
     {
-      check_bit_range(lsb, msb);
+      check_bit_range(lsb, size);
 
       bitset_t mask = ~bitset_t(0); // all bits to 1
-      mask = (mask >> (SIZE - 1 - msb)) & (mask << lsb); // set bits above msb to 0 and bits below lsb to 0
+      mask = (mask >> (SIZE - lsb - size)) & (mask << lsb); // set bits above msb to 0 and bits below lsb to 0
 
       return mask;
     }
 
-    // reverse bits between [lsb, msb]
-    void reverse_bits(bitset_t &bits, const size_t lsb, const size_t msb) const
+    // get lsb value
+    float get_lsb_value(const size_t size, const float msb_value) const
     {
-      check_bit_range(lsb, msb);
+      float lsb_value = msb_value != 0.0 ? std::abs(msb_value) / (1 << (size - 1)) : 1.0;
 
-      const size_t n_bits = msb - lsb + 1;
+      return lsb_value;
+    }
 
-      for (size_t i = 0; i < (n_bits / 2); i++)
+    // reverse bits in a bit interval
+    void reverse_bits(bitset_t &bits, const size_t lsb, const size_t size) const
+    {
+      check_bit_range(lsb, size);
+
+      for (size_t i = 0; i < (size / 2); i++)
       {
+	size_t msb = lsb + size - 1;
         bool bit = bits[lsb + i];
 
         bits[lsb + i] = bits[msb - i];
--- src/main.cpp
+++ src/main.cpp
@@ -14,18 +14,23 @@ int main(int argc, char **argv)
 {
   Word word;
 
-  size_t lsb = 0;
-  size_t msb = 15;
-  float msb_value = -180.0;
-  float value = 180.0;
+  //size_t lsb = 0;
+  //size_t size = 16;
+  //float msb_value = -180.0;
+  //float value = 180.0;
+
+  size_t lsb = 2;
+  size_t size = 8;
+  float msb_value = -10.0;
+  float value = -1.0;
 
   // profile put_data example
-  word.put_data<float>(value, lsb, msb, msb_value);
+  word.put_data<float>(value, lsb, size, msb_value);
 
   std::cout << "in value = " << value << "\n";
   std::cout << word << "\n";
-  std::cout << word.to_mil() << " " << word.to_umil() << "\n";
-  std::cout << "out value = " << word.get_data<float>(lsb, msb, msb_value) << "\n";
+  std::cout << "mil = " << word.to_mil() << "\n";
+  std::cout << "out value = " << word.get_data<float>(lsb, size, msb_value) << "\n";
 
   return 0;
 }
