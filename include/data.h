62c8da9ef0192bfff0928b93126fdbf041de2f70
Stefano Gallorini <himens@gmail.com>
2022-07-09 04:04:34 +0200

    Word class generalized to Data class (renamed header).

---
+++ include/data.h
@@ -0,0 +1,208 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <cstring>
+#include <byteswap.h>
+#include <bitset>
+#include <typeinfo>
+
+#ifndef _DATA_H_
+#define _DATA_H_
+
+typedef uint32_t mil_t;
+
+////////////////
+// Data class //
+////////////////
+template <size_t N>
+class Data 
+{
+  public:
+    static const size_t SIZE = N;
+    typedef std::bitset<SIZE> bitset_t;
+
+    // constructors
+    Data() {}
+
+    template <class T>
+      Data(const T &data) 
+      {
+        static_assert(std::is_pod<T>::value, "Cannot create Data with this data type!");
+
+        _data = data; 
+      }  
+     
+    template <class T>
+      Data(const T *data, const size_t lsb, const size_t size) 
+      {
+        static_assert(std::is_pod<T>::value, "Cannot create Data with this data type!");
+	check_bit_range(lsb, size);
+
+        const size_t size_of_T = 8 * sizeof(T);
+        const size_t number_of_elements = (size / size_of_T) + (size % size_of_T > 0);
+
+        for (size_t i = 0; i < number_of_elements; i++)
+        {
+          const bitset_t bits = data[i];
+          _data |= (bits << i * size_of_T);
+        }
+
+        data &= get_mask(lsb, size);
+      }
+
+    // print data
+    friend std::ostream& operator<<(std::ostream& os, const Data &data) 
+    {
+      os << "Data bits: " << data.get_data();
+
+      return os;
+    }
+
+    // get data bits
+    bitset_t get_data() const { return _data; }
+
+    // reset data bits to zero
+    void reset() { _data.reset(); }
+
+    // swap data bytes
+    void swap_bytes() 
+    { 
+      if      (SIZE == 16) _data = __bswap_16(_data.to_ulong()); 
+      else if (SIZE == 32) _data = __bswap_32(_data.to_ulong()); 
+      else if (SIZE == 64) _data = __bswap_64(_data.to_ulong()); 
+      else 
+      {
+        std::cout << "[WARNING] Byte swap not supported for SIZE = " << SIZE << "! \n"; 
+      }
+    } 
+
+    // reverse bits 
+    void reverse_bits(const size_t lsb = 0, const size_t size = SIZE) 
+    {
+      check_bit_range(lsb, size);
+
+      for (size_t i = 0; i < (size / 2); i++)
+      {
+	const size_t msb = lsb + size - 1;
+	const bool bit = _data[lsb + i];
+
+	_data[lsb + i] = _data[msb - i];
+	_data[msb - i] = bit;
+      }
+    }
+
+    // get data to MIL type 
+    mil_t to_mil() const { return _data.to_ulong(); }
+
+    // get data to type T
+    template <class T>
+      T get(const size_t lsb, const size_t size, const float msb_value) const 
+      {
+        static_assert(std::is_pod<T>::value, "Cannot get this data type!");
+	check_bit_range(lsb, size);
+
+	bitset_t bits = _data & get_mask(lsb, size); // extract bits 
+	bits >>= lsb; // shift back to bit 0
+
+	const bool is_signed = msb_value < 0;
+	const float lsb_value = get_lsb_value(size, msb_value);
+	const mil_t mil = bits.to_ulong();
+	T data = to_int(mil, size, is_signed) * lsb_value;
+
+	return data;
+      }
+
+    // put data to word in MIL format
+    template <class T>
+      void put(const T &data, const size_t lsb, const size_t size, const float msb_value)
+      {
+        static_assert(std::is_pod<T>::value, "Cannot put this data type!");
+	check_bit_range(lsb, size);
+
+	const bool is_signed = msb_value < 0;
+	const float lsb_value = get_lsb_value(size, msb_value);
+	const int scaled_data = data / lsb_value;
+	const mil_t mil = to_mil(scaled_data, size, is_signed);
+
+	bitset_t bits = mil;
+	bits = (bits << lsb) & get_mask(lsb, size); // shift bits by lsb and mask the rest
+
+	_data |= bits;
+      }
+
+  protected:
+    // check bit range 
+    void check_bit_range(const size_t lsb, const size_t size) const
+    {
+      const size_t msb = lsb + size - 1;
+
+      if (lsb > msb) throw std::range_error("Data::check_bit_range: lsb greater than msb!");
+      if (lsb < 0 || msb > (SIZE - 1)) throw std::out_of_range("Data::check_bit_range: lsb or msb out-of-range!");
+    }
+
+    // get mask 
+    bitset_t get_mask(const size_t lsb, const size_t size) const
+    {
+      check_bit_range(lsb, size);
+
+      bitset_t mask = ~0; // all bits to 1
+      mask = (mask >> (SIZE - lsb - size)) & (mask << lsb); // set bits above msb and below lsb to 0
+
+      return mask;
+    }
+
+    // get lsb value
+    float get_lsb_value(const size_t size, const float msb_value) const
+    {
+      if (size == 0) throw std::invalid_argument("Data::get_lsb_value: size is zero!");
+
+      float lsb_value = msb_value != 0 ? std::abs(msb_value) / pow2(size - 1) : 1;
+
+      return lsb_value;
+    }
+
+    // convert data from int to mil_t 
+    mil_t to_mil(int value, const size_t size, const bool is_signed) const
+    {
+      if (size == 0) throw std::invalid_argument("Data::to_mil: size is zero!");
+
+      const int min = is_signed ? -pow2(size - 1) : 0; 
+      const int max = is_signed ? pow2(size - 1) - 1 : pow2(size) - 1; 
+
+      if (value < min) value = min;
+      if (value > max) value = max;
+
+      mil_t mil = value < 0 ? value + pow2(size) : value;
+
+      return mil;
+    }
+
+    // convert data from mil_t to int 
+    int to_int(mil_t mil, const size_t size, const bool is_signed) const
+    {
+      if (size == 0) throw std::invalid_argument("Data::to_int: size is zero!");
+
+      const mil_t positive_int_max = pow2(size - 1) - 1;
+      const bool is_negative = is_signed && mil > positive_int_max;
+      const mil_t min = is_negative ? positive_int_max + 1 : 0; 
+      const mil_t max = !is_signed || is_negative ? pow2(size) - 1 : positive_int_max; 
+
+      if (mil < min) mil = min;
+      if (mil > max) mil = max;
+
+      int value = is_negative ? mil - pow2(size) : mil;
+
+      return value;
+    }
+
+    // fast power of 2 
+    unsigned int pow2(const size_t size) const { return (1 << size); }
+
+    // data members
+    bitset_t _data;  
+};
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -7,47 +7,77 @@
 #include <memory>
 #include <algorithm>
 
-#include "word.h"
+#include "data.h"
 #include "timer.h"
+
+const size_t WORD_SIZE = 16;
+
+typedef Data<WORD_SIZE> Word;
+typedef Data<2 * WORD_SIZE> BusData;
+
+///////////////////
+// BusData class //
+///////////////////
+//class BusData : public Data<2 * Word::SIZE>
+//{
+//  public:
+//    using Data::Data;
+//
+//    static const size_t NUMBER_OF_WORDS = 2;
+//
+//    // get data word 
+//    Word get_word(const size_t i) const 
+//    {
+//      if (i >= NUMBER_OF_WORDS) throw std::out_of_range("BusData::get_word: index out-of-range!");
+//
+//      bitset_t bits = _data & get_mask(i * Word::SIZE, Word::SIZE);
+//      bits >>= i * Word::SIZE;
+//      Word word = bits.to_ulong();
+//
+//      return word;
+//    }
+//};
 
 int main(int argc, char **argv)
 {
-  Timer timer;
-  Word word;
+  uint16_t *dest = new uint16_t[10];
 
-  //size_t lsb = 0;
-  //size_t size = 16;
-  //float msb_value = -180.0;
-  //float value = 180.0;
+  Timer timer;
+  BusData data;
 
   // profile example
   size_t lsb = 0;
-  size_t size = 16;
-  float msb_value = -10.0;
-  float value = 1.3;
+  size_t size = 19;
+  float msb_value = 32.0;
+  float value = 72.1271;
 
   float out_value = 0;
   timer.set_name("example");
   timer.start();
   {
-    word.put_data<float>(value, lsb, size, msb_value);
-    out_value = word.get_data<float>(lsb, size, msb_value);
+    data.put<float>(value, lsb, size, msb_value);
+    out_value = data.get<float>(lsb, size, msb_value);
+
+    dest[0] |= data.to_mil();
+    dest[1] |= (data.to_mil() >> WORD_SIZE);
   }
   timer.stop();
 
-  std::cout << "msb_value = " << msb_value << "\n";
   std::cout << "in value = " << value << "\n";
-  std::cout << word << "\n";
-  std::cout << "mil = " << word.to_mil() << "\n";
   std::cout << "out value = " << out_value << "\n";
+  std::cout << data << "\n";
+  std::cout << "msb_value = " << msb_value << "\n";
+  std::cout << "mil = " << data.to_mil() << "\n";
+  std::cout << "dest = " << dest[0] << " " << dest[1] << "\n";
   std::cout << timer << "\n";
 
-  word.reset();
-  word.put_data<char>('d', 0, 8, 0);
-  word.put_data<char>('i', 8, 8, 0);
-  char d = word.get_data<char>(0, 8, 0);
-  char i = word.get_data<char>(8, 8, 0);
+  data.reset();
+  data.put<char>('d', 0, 8, 0);
+  data.put<char>('i', 8, 8, 0);
+  char d = data.get<char>(0, 8, 0);
+  char i = data.get<char>(8, 8, 0);
   std::cout << d << " " << i << "\n";
 
   return 0;
 }
+
