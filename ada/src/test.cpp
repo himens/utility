00d47df63eb1c8e30bb74434e4c23e6ce0e7164f
Stefano Gallorini <himens@gmail.com>
2022-09-10 19:25:38 +0200

    Used different files for clock experiment.

--- ada/src/test.cpp
+++ ada/src/test.cpp
@@ -4,6 +4,7 @@ extern "C"
 {
   void get_mil(Data::word_t *point);
   long int my_clock();
+  int my_clock_gettime (clockid_t __clock_id, struct timespec *__tp) __THROW;
 }
 
 struct data_t {
@@ -36,4 +37,14 @@ long int my_clock()
 { 
   std::cout << "Uff \n"; 
   return 1; 
+}
+
+int my_clock_gettime (clockid_t __clock_id, struct timespec *__tp) __THROW
+{
+  std::cout << "Uff2 \n"; 
+
+  __tp->tv_sec = 1;
+  __tp->tv_nsec = 0;
+
+  return 0; 
 }
