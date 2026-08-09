d94a0fbd739bde20df74925fa75afcd093c56a6b
Stefano Gallorini <himens@gmail.com>
2026-08-09 13:54:04 +0000

    Refactor Timer class: streamline methods, enhance functionality, and improve readability

--- include/timer.h
+++ include/timer.h
@@ -1,59 +1,77 @@
+#pragma once
 #include <iostream>
 #include <sstream>
-#include <fstream>
-#include <string>
-#include <map>
-#include <vector>
-#include <memory>
-#include <algorithm>
 #include <thread>    
 #include <chrono>
-
-#ifndef TIMER_H
-#define TIMER_H
 
 /////////////////
 // Timer class //
 /////////////////
-class Timer
-{
-  public:
-    Timer(const std::string name) { set_name(name); }
-    Timer() {};
-
-    static void sleep_ms(const unsigned int ms) { std::this_thread::sleep_for(std::chrono::milliseconds(ms)); }
-    static void sleep_s(const unsigned int s) { std::this_thread::sleep_for(std::chrono::seconds(s)); }
-
-    friend std::ostream& operator<<(std::ostream& os, const Timer t) 
-    {
-      auto elapsed_time_us = t.get_elapsed_time_us();
-
-      os << "[Timer " + t.get_name() + "] Elapsed time = ";
-      if (elapsed_time_us < 1e3) os << elapsed_time_us << "us";
-      else if (elapsed_time_us >= 1e3 && elapsed_time_us < 1e6) os << elapsed_time_us * 1e-3 << "ms";
-      else os << elapsed_time_us * 1e-6 << "s";
-
-      return os;
-    }
-    
-    void start() { _start = std::chrono::system_clock::now(); }
-    void stop() { _end = std::chrono::system_clock::now(); }
-    void reset() { _start = _end = {}; }
-    void set_name(const std::string name) { _name = name; }
-    std::string get_name() const { return _name; }
-    double get_elapsed_time_us() const { return get_elapsed_time_s() * 1e6; }
-    double get_elapsed_time_ms() const { return get_elapsed_time_s() * 1e3; }
-    double get_elapsed_time_s() const
-    { 
-      std::chrono::duration<double> elapsed_seconds = _end - _start;
-
-      return elapsed_seconds.count();
-    }
-
-  private:  
-    std::string _name;
-    std::chrono::time_point<std::chrono::system_clock> _start;
-    std::chrono::time_point<std::chrono::system_clock> _end;
+// This class is used to measure the elapsed time. It provides methods to start, stop, and reset the timer,
+// as well as to retrieve the elapsed time in microseconds, milliseconds, or seconds. 
+// Additionally, it includes static sleep functions to pause execution for a specified duration.
+// If timer started and stopped multiple times, the elapsed time will be the sum of all intervals between start and stop calls,
+// until the timer is reset.
+class Timer {
+	public:
+	    // Sleep functions
+    	static void sleep_ms(const unsigned int ms) { 
+			std::this_thread::sleep_for(std::chrono::milliseconds(ms)); 
+		}
+    	static void sleep_s(const unsigned int s) { 
+			std::this_thread::sleep_for(std::chrono::seconds(s)); 
+		}
+        // Start, stop, reset timer
+    	void start() {
+			if (not _running) {
+				_start = std::chrono::system_clock::now() - (_end - _start);
+				_running = true;
+			}
+    	}
+    	void stop() {
+			if (_running) {
+				_end = std::chrono::system_clock::now();
+				_running = false;
+			}
+    	}
+    	void reset() {
+			if (not _running) {
+      			_start = _end = {};
+			}
+		    else {
+				_start = std::chrono::system_clock::now();
+			}
+    	}
+		// Get elapsed time
+    	double get_elapsed_time_us() const { 
+			return get_elapsed_time_s() * 1e6; 
+		}
+    	double get_elapsed_time_ms() const { 
+			return get_elapsed_time_s() * 1e3; 
+		}
+    	double get_elapsed_time_s() const { 
+        	const std::chrono::duration<double> elapsed_s{_end - _start};
+      		return elapsed_s.count();
+    	}
+		// Print timer information
+    	friend std::ostream& operator<<(std::ostream& os, const Timer t) {
+      		const auto elapsed_time_us = t.get_elapsed_time_us();
+      		os << "[Elapsed time = ";
+      		if (elapsed_time_us < 1e3) {
+				os << elapsed_time_us << "us";
+			}
+      		else if (elapsed_time_us >= 1e3 and elapsed_time_us < 1e6) {
+				os << elapsed_time_us * 1e-3 << "ms";
+			}
+      		else {
+				os << elapsed_time_us * 1e-6 << "s";
+			}
+			os << "]";
+      		return os;
+    	}    
+  	private:  
+	    // Data members
+		bool _running{false};
+    	std::chrono::time_point<std::chrono::system_clock> _start;
+    	std::chrono::time_point<std::chrono::system_clock> _end;
 };
-#endif
-
