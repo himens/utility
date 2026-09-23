5e0a7c1fb58bf6cf0a3a86808981a881486012e5
himens <himens@gmail.com>
2026-09-23 21:11:17 +0200

    Add utils namespace. Add random::generator() and random::generate().

--- include/random.h
+++ include/random.h
@@ -1,9 +1,27 @@
 #pragma once
 #include <random>
+#include <ranges>
 
-// Get random generator
-std::mt19937& random_generator() {
-    static std::random_device rd;
-    static thread_local std::mt19937 gen(rd());
-    return gen;
+namespace utils::random {
+    // Get random generator
+    std::mt19937& generator() {
+        static std::random_device rd;
+        static thread_local std::mt19937 gen(rd());
+        return gen;
+    }
+    // Generate random numbers
+    template <typename T>
+        requires(std::integral<T> or std::floating_point<T>)
+        std::vector<T> generate(const size_t size, const T min, const T max) {
+            std::vector<T> values(size);
+            if constexpr(std::integral<T>) {
+                std::uniform_int_distribution<T> distrib(min, max);
+                std::ranges::generate(values, [&]() { return distrib(generator()); });
+            }
+            else if constexpr(std::floating_point<T>) {
+                std::uniform_real_distribution<T> distrib(min, max);
+                std::ranges::generate(values, [&]() { return distrib(generator()); });
+            }
+            return values;
+        }
 }
--- include/timer.h
+++ include/timer.h
@@ -2,59 +2,61 @@
 #include <thread>    
 #include <chrono>
 
-/////////////////
-// Timer class //
-/////////////////
-// This class is used to measure the elapsed time. It provides methods to start, stop, and reset the timer,
-// as well as to retrieve the elapsed time in microseconds, milliseconds, or seconds. 
-// Additionally, it includes static sleep functions to pause execution for a specified duration.
-// If timer started and stopped multiple times, the elapsed time will be the sum of all intervals between start and stop calls,
-// until the timer is reset.
-class Timer {
-	public:
-	    // Sleep functions
-    	static void sleep_ms(const unsigned int ms) { 
-			std::this_thread::sleep_for(std::chrono::milliseconds(ms)); 
-		}
-    	static void sleep_s(const unsigned int s) { 
-			std::this_thread::sleep_for(std::chrono::seconds(s)); 
-		}
-        // Start, stop, reset timer
-    	void start() {
-			if (not _running) {
-				_start = std::chrono::system_clock::now() - (_end - _start);
-				_running = true;
-			}
-    	}
-    	void stop() {
-			if (_running) {
-				_end = std::chrono::system_clock::now();
-				_running = false;
-			}
-    	}
-    	void reset() {
-			if (not _running) {
-      			_start = _end = {};
-			}
-		    else {
-				_start = std::chrono::system_clock::now();
-			}
-    	}
-		// Get elapsed time
-    	double get_elapsed_time_us() const { 
-			return get_elapsed_time_s() * 1e6; 
-		}
-    	double get_elapsed_time_ms() const { 
-			return get_elapsed_time_s() * 1e3; 
-		}
-    	double get_elapsed_time_s() const { 
-            const auto end = _running ? std::chrono::system_clock::now() : _end;
-            const std::chrono::duration<double> elapsed_s{end - _start};
-      		return elapsed_s.count();
-    	}
-  	private:  
-	    // Data members
-		bool _running{false};
-    	std::chrono::time_point<std::chrono::system_clock> _start;
-    	std::chrono::time_point<std::chrono::system_clock> _end;
-};
+namespace utils {
+    /////////////////
+    // Timer class //
+    /////////////////
+    // This class is used to measure the elapsed time. It provides methods to start, stop, and reset the timer,
+    // as well as to retrieve the elapsed time in microseconds, milliseconds, or seconds. 
+    // Additionally, it includes static sleep functions to pause execution for a specified duration.
+    // If timer started and stopped multiple times, the elapsed time will be the sum of all intervals between start and stop calls,
+    // until the timer is reset.
+    class Timer {
+        public:
+            // Sleep functions
+            static void sleep_ms(const unsigned int ms) { 
+                std::this_thread::sleep_for(std::chrono::milliseconds(ms)); 
+            }
+            static void sleep_s(const unsigned int s) { 
+                std::this_thread::sleep_for(std::chrono::seconds(s)); 
+            }
+            // Start, stop, reset timer
+            void start() {
+                if (not _running) {
+                    _start = std::chrono::system_clock::now() - (_end - _start);
+                    _running = true;
+                }
+            }
+            void stop() {
+                if (_running) {
+                    _end = std::chrono::system_clock::now();
+                    _running = false;
+                }
+            }
+            void reset() {
+                if (not _running) {
+                    _start = _end = {};
+                }
+                else {
+                    _start = std::chrono::system_clock::now();
+                }
+            }
+            // Get elapsed time
+            double get_elapsed_time_us() const { 
+                return get_elapsed_time_s() * 1e6; 
+            }
+            double get_elapsed_time_ms() const { 
+                return get_elapsed_time_s() * 1e3; 
+            }
+            double get_elapsed_time_s() const { 
+                const auto end = _running ? std::chrono::system_clock::now() : _end;
+                const std::chrono::duration<double> elapsed_s{end - _start};
+                return elapsed_s.count();
+            }
+        private:  
+            // Data members
+            bool _running{false};
+            std::chrono::time_point<std::chrono::system_clock> _start;
+            std::chrono::time_point<std::chrono::system_clock> _end;
+    };
+}
